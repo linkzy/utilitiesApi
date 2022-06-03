cee3e3b915f6fbd633041e9098f7b8a99e25a377
Danilo Cavalcante <danilo.cavalcante@adm.unip.br>
2022-06-03 13:40:58 -0300

    Initial commit

---
+++ utilitiesApi/utilitiesApi/Controllers/RequestInformationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace utilitiesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RequestInformationController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string ip = "";
+            try
+            {
+               ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            catch (Exception)
+            {
+                ip = "not detected";
+            }
+
+            return Ok(new { 
+                Ip = "",
+                UserAgent = Request.Headers.UserAgent,
+                AcceptEncoding = Request.Headers.AcceptEncoding,
+                AcceptLanguage = Request.Headers.AcceptLanguage
+            });
+        }
+    }
+}
